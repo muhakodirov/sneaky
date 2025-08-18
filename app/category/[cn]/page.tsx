e1c72f10bca73536b4146ded0830c531af1e4eb2
Muhammad <muhammadkodirov089@gmail.com>
2025-08-18 22:53:46 +0200

    test commit in main

--- app/category/[cn]/page.tsx
+++ app/category/[cn]/page.tsx
@@ -5,7 +5,7 @@ import { use } from "react";
 
 function CategoryPage({ params }: { params: Promise<{ cn: string }> }) {
     const { cn } = use(params)
-    return <div>Hallo: {cn} </div>
+    return <div>Hallo3: {cn} </div>
 }
 
 export default CategoryPage
